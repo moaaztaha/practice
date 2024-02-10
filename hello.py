198eb5692f7441a9eb885bca00aa16fa5498b143
Moaz <moaaztaha7@gmail.com>
2024-02-10 14:43:43 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+    """
+    Adds two numbers together.
+
+    Args:
+    num1 (int): The first number
+    num2 (int): The second number
+
+    Returns:
+    int: The sum of num1 and num2
+    """
+    return num1 + num2
+
+
+print(add(1, 2))
